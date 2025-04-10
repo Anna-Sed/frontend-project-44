62c3135afcc180e280352fbba66d2b4dd0fbe982
ann-sed <a.sedeeva@yandex.ru>
2025-04-10 19:20:29 +0300

    Add to src/games/game-gcd.js new game

---
+++ src/games/game-gcd.js
@@ -0,0 +1,36 @@
+import * as _ from '../index.js';
+
+export default () => {
+  // greeting
+  _.mainGreeting('Welcome to the Brain Games!');
+  const user = _.userName();
+  _.userGreeting(user);
+
+  const creatingQuestion = () => {
+    const randomNum1 = _.getRandomInt(0, 80);
+    const randomNum2 = _.getRandomInt(0, 80);
+    const question = `Question: ${randomNum1} and ${randomNum2}`;
+
+    let divisible = Math.max(randomNum1, randomNum2);
+    let divisor = Math.min(randomNum1, randomNum2);
+    let correctAnswer;
+    let remainder = divisible % divisor;
+    if (remainder === 0) {
+      correctAnswer = divisor;
+    } else {
+      while (remainder !== 0) {
+        divisible = divisor;
+        divisor = remainder;
+        remainder = divisible % divisor;
+      }
+      correctAnswer = divisor;
+    }
+    return [question, `${correctAnswer}`];
+  };
+
+  const rule = `You need to answer 3 questions correctly.
+Find the greatest common divisor of given numbers.`;
+  const countOfCorrectAnswer = _.game(rule, creatingQuestion);
+
+  _.conclusion(user, countOfCorrectAnswer);
+};
