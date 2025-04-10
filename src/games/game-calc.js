619302f35bde5dfbc5774d5d417c8370db7ac719
ann-sed <a.sedeeva@yandex.ru>
2025-04-10 13:31:38 +0300

    Add to src/games/game-calc.js new game

---
+++ src/games/game-calc.js
@@ -0,0 +1,39 @@
+import * as _ from '../index.js';
+
+export default () => {
+  // greeting
+  _.mainGreeting('Welcome to the Brain Games!');
+  const user = _.userName();
+  _.userGreeting(user);
+
+  const creatingQuestion = () => {
+    const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+    const randomNum1 = getRandomInt(0, 80);
+    const randomNum2 = getRandomInt(0, 80);
+    const operations = ['+', '-', '*'];
+    const currentOperation = operations.at(getRandomInt(0, operations.length - 1));
+    const question = `Question: ${randomNum1} ${currentOperation} ${randomNum2}`;
+
+    let correctAnswer = 0;
+    switch (currentOperation) {
+      case '+':
+        correctAnswer = randomNum1 + randomNum2;
+        break;
+      case '-':
+        correctAnswer = randomNum1 - randomNum2;
+        break;
+      case '*':
+        correctAnswer = randomNum1 * randomNum2;
+        break;
+      default:
+        correctAnswer = 'We don\'t have this operation';
+    }
+    return [question, `${correctAnswer}`];
+  };
+
+  const rule = `You need to answer 3 questions correctly.
+What is result of the expression?`;
+  const countOfCorrectAnswer = _.game(rule, creatingQuestion);
+
+  _.conclusion(user, countOfCorrectAnswer);
+};
