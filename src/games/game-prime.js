99704547d8f9ed603bd4078739bdb139813f37ca
ann-sed <a.sedeeva@yandex.ru>
2025-04-18 17:02:20 +0300

    Fix the main logic of the game src/games/game-prime.js

--- src/games/game-prime.js
+++ src/games/game-prime.js
@@ -1,29 +1,22 @@
-import * as _ from '../index.js';
+import { getRandomInt, game } from '../index.js';
 
+const isPrime = (num) => {
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
 export default () => {
-  // greeting
-  _.mainGreeting('Welcome to the Brain Games!');
-  const user = _.userName();
-  _.userGreeting(user);
-
-  const creatingGameData = () => {
-    const randomNum = _.getRandomInt(2, 29);
+  // create question for game`s round and correct answer
+  const creatingDataGame = () => {
+    const randomNum = getRandomInt(2, 67);
     const question = `Question: ${randomNum}`;
-
-    const isPrime = (num) => {
-      for (let i = 2; i < num; i += 1) {
-        if (num % i === 0) {
-          return false;
-        }
-      }
-      return true;
-    };
     const correctAnswer = isPrime(randomNum) ? 'yes' : 'no';
     return [question, correctAnswer];
   };
-
+  // run the game
   const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-  const countOfCorrectAnswer = _.game(rule, creatingGameData);
-
-  _.conclusion(user, countOfCorrectAnswer);
+  game(rule, creatingDataGame);
 };
