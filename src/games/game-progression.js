dd498eff19cb697b256f44de3f3e7fd11843f26c
ann-sed <a.sedeeva@yandex.ru>
2025-04-18 17:01:23 +0300

    Fix the main logic of the game src/games/game-progression.js

--- src/games/game-progression.js
+++ src/games/game-progression.js
@@ -1,31 +1,28 @@
-import * as _ from '../index.js';
+import { getRandomInt, game } from '../index.js';
 
-export default () => {
-  // greeting
-  _.mainGreeting('Welcome to the Brain Games!');
-  const user = _.userName();
-  _.userGreeting(user);
-
-  // create question for game`s round and correct answer
-  const creatingGameData = () => {
-    let startingNum = _.getRandomInt(0, 75);
-    const difference = _.getRandomInt(2, 7);
-    const progression = [];
+const makeProgression = () => {
+  let startingNum = getRandomInt(0, 75);
+  const difference = getRandomInt(2, 7);
+  const progression = [];
+  progression.push(startingNum);
+  while (progression.length !== 10) {
+    startingNum += difference;
     progression.push(startingNum);
-    while (progression.length !== 10) {
-      startingNum += difference;
-      progression.push(startingNum);
-    }
-    const randomIndex = _.getRandomInt(1, 9);
+  }
+  return progression;
+};
+export default () => {
+  // create question for game`s round and correct answer
+  const creatingDataGame = () => {
+    const progression = makeProgression();
+    const randomIndex = getRandomInt(1, 9);
     const correctAnswer = progression[randomIndex].toString();
 
     progression[randomIndex] = '..';
     const question = `Question: ${progression.join(' ')}`;
     return [question, correctAnswer];
   };
-
+  // run the game
   const rule = 'What number is missing in the progression?';
-  const countOfCorrectAnswer = _.game(rule, creatingGameData);
-
-  _.conclusion(user, countOfCorrectAnswer);
+  game(rule, creatingDataGame);
 };
