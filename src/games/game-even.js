0e51718cdbd7c99a246a696b9f22db9215688e3b
ann-sed <a.sedeeva@yandex.ru>
2025-04-18 17:02:54 +0300

    Fix the main logic of the game src/games/game-even.js

--- src/games/game-even.js
+++ src/games/game-even.js
@@ -1,23 +1,16 @@
-import * as _ from '../index.js';
+import { getRandomInt, game } from '../index.js';
+
+const isEven = (num) => num % 2 === 0;
 
 export default () => {
-  // greeting
-  _.mainGreeting('Welcome to the Brain Games!');
-  const user = _.userName();
-  _.userGreeting(user);
-
   // create question for game`s round and correct answer
-  const creatingGameData = () => {
-    const randomNum = _.getRandomInt(0, 100);
+  const creatingDataGame = () => {
+    const randomNum = getRandomInt(0, 100);
     const question = `Question: ${randomNum}`;
-
-    const isEven = (num) => num % 2 === 0;
     const correctAnswer = isEven(randomNum) ? 'yes' : 'no';
     return [question, correctAnswer];
   };
-
+  // run the game
   const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
-  const countOfCorrectAnswer = _.game(rule, creatingGameData);
-
-  _.conclusion(user, countOfCorrectAnswer);
+  game(rule, creatingDataGame);
 };
