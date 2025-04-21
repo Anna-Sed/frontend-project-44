a809e9cbf1555ca848678b014c8bded6e92b8221
ann-sed <a.sedeeva@yandex.ru>
2025-04-21 18:49:54 +0300

    Add to src/games/game-gcd.js the new function

--- src/games/game-gcd.js
+++ src/games/game-gcd.js
@@ -1,35 +1,25 @@
-import * as _ from '../index.js';
+import { getRandomInt, game } from '../index.js';
+
+const calculateGcdResult = (num1, num2) => {
+  const divisible = Math.max(num1, num2);
+  const divisor = Math.min(num1, num2);
+  const remainder = divisible % divisor;
+  if (remainder > 0) {
+    return calculateGcdResult(divisor, remainder);
+  }
+  return divisor;
+};
 
 export default () => {
-  // greeting
-  _.mainGreeting('Welcome to the Brain Games!');
-  const user = _.userName();
-  _.userGreeting(user);
-
   const creatingGameData = () => {
-    const randomNum1 = _.getRandomInt(0, 40);
-    const randomNum2 = _.getRandomInt(0, 40);
+    const randomNum1 = getRandomInt(1, 40);
+    const randomNum2 = getRandomInt(1, 40);
     const question = `Question: ${randomNum1} ${randomNum2}`;
 
-    let divisible = Math.max(randomNum1, randomNum2);
-    let divisor = Math.min(randomNum1, randomNum2);
-    let correctAnswer;
-    let remainder = divisible % divisor;
-    if (remainder === 0) {
-      correctAnswer = divisor;
-    } else {
-      while (remainder !== 0) {
-        divisible = divisor;
-        divisor = remainder;
-        remainder = divisible % divisor;
-      }
-      correctAnswer = divisor;
-    }
+    const correctAnswer = calculateGcdResult(randomNum1, randomNum2);
     return [question, `${correctAnswer}`];
   };
 
   const rule = 'Find the greatest common divisor of given numbers.';
-  const countOfCorrectAnswer = _.game(rule, creatingGameData);
-
-  _.conclusion(user, countOfCorrectAnswer);
+  game(rule, creatingGameData);
 };
