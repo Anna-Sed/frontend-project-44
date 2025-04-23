ce9f3b6ef59b1c6ea6218e843d68878f1eba3cd0
ann-sed <a.sedeeva@yandex.ru>
2025-04-23 14:44:48 +0300

    Fix to src/games/game-calc.js the function

--- src/games/game-calc.js
+++ src/games/game-calc.js
@@ -18,19 +18,20 @@ const calculateArithmeticResult = (operation, num1, num2) => {
   return result.toString();
 };
 
-export default () => {
-  // create question for game`s round and correct answer
-  const creatingDataGame = () => {
-    const randomNum1 = getRandomInt(0, 50);
-    const randomNum2 = getRandomInt(0, 40);
-    const operations = ['+', '-', '*'];
-    const currentOperation = operations.at(getRandomInt(0, operations.length - 1));
-    const question = `Question: ${randomNum1} ${currentOperation} ${randomNum2}`;
+// create question for game`s round and correct answer
+const createDataGame = () => {
+  const randomNum1 = getRandomInt(0, 50);
+  const randomNum2 = getRandomInt(0, 40);
+  const operations = ['+', '-', '*'];
+  const currentOperation = operations.at(getRandomInt(0, operations.length - 1));
+  const question = `Question: ${randomNum1} ${currentOperation} ${randomNum2}`;
 
-    const correctAnswer = calculateArithmeticResult(currentOperation, randomNum1, randomNum2);
-    return [question, correctAnswer];
-  };
+  const correctAnswer = calculateArithmeticResult(currentOperation, randomNum1, randomNum2);
+  return [question, correctAnswer];
+};
+
+export default () => {
   // run the game
   const rule = 'What is the result of the expression?';
-  game(rule, creatingDataGame);
+  game(rule, createDataGame);
 };
