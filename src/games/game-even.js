394f91fae9cb342f4a8c9289030b73fd5832cd3d
ann-sed <a.sedeeva@yandex.ru>
2025-04-23 14:57:02 +0300

    Fix to  src/games/game-even.js the function

--- src/games/game-even.js
+++ src/games/game-even.js
@@ -2,15 +2,15 @@ import { getRandomInt, game } from '../index.js';
 
 const isEven = (num) => num % 2 === 0;
 
+// create question for game`s round and correct answer
+const createDataGame = () => {
+  const randomNum = getRandomInt(0, 100);
+  const question = `Question: ${randomNum}`;
+  const correctAnswer = isEven(randomNum) ? 'yes' : 'no';
+  return [question, correctAnswer];
+};
 export default () => {
-  // create question for game`s round and correct answer
-  const creatingDataGame = () => {
-    const randomNum = getRandomInt(0, 100);
-    const question = `Question: ${randomNum}`;
-    const correctAnswer = isEven(randomNum) ? 'yes' : 'no';
-    return [question, correctAnswer];
-  };
   // run the game
   const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
-  game(rule, creatingDataGame);
+  game(rule, createDataGame);
 };
