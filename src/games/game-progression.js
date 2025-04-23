cb3855f1e30330749fff7bb7d635823b51cc95de
ann-sed <a.sedeeva@yandex.ru>
2025-04-23 14:37:33 +0300

    Fix to src/games/game-progression.js functions

--- src/games/game-progression.js
+++ src/games/game-progression.js
@@ -1,8 +1,7 @@
 import { getRandomInt, game } from '../index.js';
 
-const makeProgression = () => {
-  let startingNum = getRandomInt(0, 75);
-  const difference = getRandomInt(2, 7);
+const makeProgression = (start, difference) => {
+  let startingNum = start;
   const progression = [];
   progression.push(startingNum);
   while (progression.length !== 10) {
@@ -11,18 +10,22 @@ const makeProgression = () => {
   }
   return progression;
 };
-export default () => {
-  // create question for game`s round and correct answer
-  const creatingDataGame = () => {
-    const progression = makeProgression();
-    const randomIndex = getRandomInt(1, 9);
-    const correctAnswer = progression[randomIndex].toString();
 
-    progression[randomIndex] = '..';
-    const question = `Question: ${progression.join(' ')}`;
-    return [question, correctAnswer];
-  };
+// create question for game`s round and correct answer
+const createDataGame = () => {
+  const startingNum = getRandomInt(0, 75);
+  const difference = getRandomInt(2, 7);
+  const progression = makeProgression(startingNum, difference);
+  const randomIndex = getRandomInt(1, 9);
+  const correctAnswer = progression[randomIndex].toString();
+
+  progression[randomIndex] = '..';
+  const question = `Question: ${progression.join(' ')}`;
+  return [question, correctAnswer];
+};
+
+export default () => {
   // run the game
   const rule = 'What number is missing in the progression?';
-  game(rule, creatingDataGame);
+  game(rule, createDataGame);
 };
